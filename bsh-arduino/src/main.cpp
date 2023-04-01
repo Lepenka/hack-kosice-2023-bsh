12e9c49a47e427d257da34d8e512401907857eea
Eduard Hopfer <core@ddcveng.cc>
2023-04-01 18:23:58 +0200

    heureka

--- bsh-arduino/src/main.cpp
+++ bsh-arduino/src/main.cpp
@@ -20,42 +20,13 @@ void setup() {
 
 
 void loop() {
-  byte error, address;
-  int nDevices;
+  const byte device_1 = 0x5C;
+  const byte device_enable = 0x01;
 
-  Serial.println("Scanning...");
+  Wire.beginTransmission(device_1);
+  Wire.write(device_enable);
+  Wire.write(0x01);
+  Wire.endTransmission();
 
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ) 
-  {
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    WIRE.beginTransmission(address);
-    error = WIRE.endTransmission();
-
-    if (error == 0)
-    {
-      Serial.print("I2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-
-      nDevices++;
-    }
-    else if (error==4) 
-    {
-      Serial.print("Unknown error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-  if (nDevices == 0)
-    Serial.println("No I2C devices found\n");
-  else
-    Serial.println("done\n");
-
-  delay(5000);           // wait 5 seconds for next scan
+  delay(1000);           // wait 5 seconds for next scan
 }
