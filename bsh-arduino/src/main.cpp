10c0e6caf20024d2b9709fc63a30637cd66437f9
Kristián Petráš <kpetras@tutanota.com>
2023-04-02 01:39:15 +0200

    Added toggling of leds

--- bsh-arduino/src/main.cpp
+++ bsh-arduino/src/main.cpp
@@ -21,6 +21,7 @@ enum Values {
 };
 
 enum ColorType {
+  NONE = 0,
   RED = 1,
   GREEN = 2,
   BLUE = 4,
@@ -30,6 +31,11 @@ enum ColorType {
   RED_GREEN_BLUE = 7,
 };
 
+const byte ROWS = 4;
+const byte COLUMNS = 4;
+
+ColorType channels[ROWS][COLUMNS];
+
 void command(RowSelection selection, CommandType command_type, byte value) {
   Wire.beginTransmission(selection);
   Wire.write(command_type);
@@ -37,11 +43,23 @@ void command(RowSelection selection, CommandType command_type, byte value) {
   Wire.endTransmission();
 }
 
+void init_channels() {
+  for (byte i = 0; i < ROWS; i++)
+  {
+    for (byte j = 0; j < COLUMNS; j++)
+    {
+      channels[i][j] = ColorType::NONE;
+    }
+  }
+  Serial.println("Channels initialized");
+}
+
 void init_transfer() {
   Wire.begin();
   Serial.begin(9600);
   while (!Serial)
     delay(100);
+  delay(1000);
   Serial.println("Arduino initialized");
 }
 
@@ -62,6 +80,8 @@ RowSelection get_row(byte x) {
         return RowSelection::THIRD;
       case 3:
         return RowSelection::FOURTH;
+      default:
+        return RowSelection::ALL;
   }
 }
 
@@ -71,6 +91,35 @@ ColorType get_color_type(size_t r, size_t g, size_t b) {
   if (g > 0) value |= GREEN;
   if (b > 0) value |= BLUE;
   return (ColorType)value;
+}
+
+ColorType diff_color_types(ColorType source, ColorType removal) {
+  byte mask = ~((byte)removal);
+  byte to_remove = (byte)source & mask;
+  byte result = (byte)source - to_remove;
+  return (ColorType)result;
+}
+
+void clear_led(byte x, byte y, ColorType color_type) {
+  RowSelection row = get_row(x);
+
+  ColorType old_color = channels[x][y];
+  ColorType new_color = diff_color_types(old_color, color_type);
+
+  channels[x][y] = new_color;
+
+  int mask = 0;
+  for (byte i = 0; i < COLUMNS; i++)
+  {
+    mask += channels[x][i];
+    mask <<= 3;
+  }
+  
+  byte low = mask & 0xFF;
+  byte high = (mask >> 8) & 0xFF;
+
+  command(row, CommandType::CHANNEL_HIGH_TOGGLE, high);
+  command(row, CommandType::CHANNEL_LOW_TOGGLE, low);
 }
 
 void toggle_led(byte x, byte y, size_t r, size_t g, size_t b) {
@@ -83,6 +132,8 @@ void toggle_led(byte x, byte y, size_t r, size_t g, size_t b) {
 
   command(row, CommandType::CHANNEL_HIGH_TOGGLE, high);
   command(row, CommandType::CHANNEL_LOW_TOGGLE, low);
+
+  channels[x][y] = color_type;
 }
 
 // byte get_pattern(byte x, byte y) {
@@ -97,8 +148,8 @@ void color_led(byte x, byte y, size_t r, size_t g, size_t b) {
 void setup() {
   init_transfer();
   init_led();
+  init_channels();
 }
-
 
 void loop() {
 }
